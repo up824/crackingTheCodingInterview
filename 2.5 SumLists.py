214090882b0257c8c1a0a8cfae5fd4e44b25a93b
Yuan Zhou <yuanzhou@qti.qualcomm.com>
2018-04-04 17:35:54 -0700

    Q2.5 init

---
+++ 2.5 SumLists.py
@@ -0,0 +1,26 @@
+# 2.5 Sum from LSB to MSB and from MSB to LSB
+class ListNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.node = None
+
+def sumList(a, b):
+    dummy = ListNode(-1)
+    head = dummy
+    carry = 0
+    while a or b or carry:
+        valA = valB = 0
+        if a:
+            valA = a.val
+            a = a.next
+        if b:
+            valB = b.val
+            b = b.next
+
+        sumVal = valA + valB + carry
+        carry, val = sumVal / 10, sumVal % 10
+        head.next = ListNode(val)
+
+        head = head.next
+
+    return dummy.next
