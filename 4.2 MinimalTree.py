223fb803dac2e3f6a8c80170ac84b2e8712a855b
Yuan Zhou <yuanzhou@qti.qualcomm.com>
2018-04-06 19:54:21 -0700

    added Q4.2

---
+++ 4.2 MinimalTree.py
@@ -0,0 +1,16 @@
+# 4.3 Minimal Tree
+class TreeNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def minimalTree(arr):
+    if not arr:
+        return None
+
+    m = len(arr) / 2
+    root = TreeNode(arr[m])
+    root.left = self.minimalTree(arr[:m])
+    root.right = self.minimalTree(arr[m + 1:])
+    return root
