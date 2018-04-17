ca33d03fabcaa01a186e655b7539cb8f13bcdce8
Yuan Zhou <yuanzhou@qti.qualcomm.com>
2018-04-16 21:34:48 -0700

    added Binary search solution to 8.3

--- 8.3 MagicIndex.py
+++ 8.3 MagicIndex.py
@@ -4,3 +4,15 @@ def magicIndex(arr):
         if i == num:
             return True
     return False
+
+def magicIndex(arr):
+    start, end = 0, len(arr) - 1
+    while start <= end:
+        mid = start + (end - start) // 2
+        if arr[mid] == mid:
+            return True
+        if arr[mid] > mid:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return False
