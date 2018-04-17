aed2ccb6dc8aca705cc0901d75e512c3d4bd4124
Yuan Zhou <yuanzhou@qti.qualcomm.com>
2018-04-16 17:55:09 -0700

    added 6.9 6.10

---
+++ 6.10 Poison.py
@@ -0,0 +1,6 @@
+#6.10 Poison
+"""
+binary search
+use 10 strips at the same time, each one cover different portion of the bottles.
+like bit in a 10-bit var
+"""
---
+++ 6.9 100Lockers.py
@@ -0,0 +1,6 @@
+#6.9 100 locks
+def oneHundredLocks(n):
+    i = 1
+    while i * i <= 100:
+        i += 1
+    return i - 1
